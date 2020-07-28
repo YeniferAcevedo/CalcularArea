b90dc105ab5b4bda1eef77ba175fa276afe82984
YeniferAcevedo <yeniferacevedo@htmail.com>
2020-07-28 00:24:17 -0500

    Se crea programa que permite calcular el área de un círculo

---
+++ Calcular Area/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Calcular_Area
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtradio = new System.Windows.Forms.TextBox();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(413, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calcula el área del círculo!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese radio del círculo:";
+            // 
+            // txtradio
+            // 
+            this.txtradio.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtradio.Location = new System.Drawing.Point(275, 109);
+            this.txtradio.Name = "txtradio";
+            this.txtradio.Size = new System.Drawing.Size(100, 26);
+            this.txtradio.TabIndex = 2;
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(271, 174);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(18, 19);
+            this.lblresultado.TabIndex = 3;
+            this.lblresultado.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "El área del círculo es:";
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(427, 103);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(78, 32);
+            this.btncalcular.TabIndex = 5;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Calcular_Area.Properties.Resources.maxresdefault;
+            this.pictureBox1.Location = new System.Drawing.Point(200, 242);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(352, 218);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpiar.Location = new System.Drawing.Point(39, 242);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(83, 32);
+            this.btnlimpiar.TabIndex = 7;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(579, 485);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.txtradio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Calcular Área";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtradio;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnlimpiar;
+    }
+}
+
---
+++ Calcular Area/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calcular_Area
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            Double radio = Double.Parse(txtradio.Text);
+            Double area = 0;
+
+            area = Math.PI * radio * radio;
+
+            lblresultado.Text = Convert.ToString(area);
+
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtradio.Text = String.Empty;
+            lblresultado.Text = "0";
+            txtradio.Focus();
+        }
+    }
+}
